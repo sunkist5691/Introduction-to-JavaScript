1b804e2ccbef73fedcbf2b2dcb798f9b15f651c7
sunkist5691 <66539462+sunkist5691@users.noreply.github.com>
2020-07-24 13:37:52 -0700

    Introduction to Javascript

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
+let votingAge = 21;
 
-
-
-
+if (votingAge > 18) {
+  console.log(true);
+}
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
+let var1 = 0;
+let var2 = 1;
 
+if (var1 === 1) {
+  var2 = 0;
+} else {
+  var2 = 1;
+}
+console.log(var2);
 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
-
-
-
-
+let number = "1999";
+console.log(parseInt(number));
 
 //Task d: Write a function to multiply a*b 
 
+function multiply(a, b) {
+  return a * b;
+}
 
 
 
@@ -27,8 +37,10 @@
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
-
-
+function dogYears(myAge) {
+  return myAge * 7;
+}
+console.log(dogYears(27));
 
 
 /************************************************************** Task 3 **************************************************************/
@@ -48,8 +60,50 @@
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
 
+function dogFeeder (weight, age) 
+{
+  if (age >= 1) 
+  {
+    if(weight <= 5)
+    {
+      return weight * .05;
+    } 
+    else if (weight > 5 && weight <= 10)
+    {
+      return weight * .04;
+    }
+    else if (weight > 10 && weight <= 15) 
+    {
+      return weight * .03;
+    }
+    else 
+    {
+      return weight * .02;
+    }
+  } 
+  else 
+  {
+    if (age >= (1/6) && age <= (1/4))
+    {
+      return weight * .1;
+    }
+    else if (age > (1/4) && age <= (7/12))
+    {
+      return weight * .05;
+    }
+    else if (age > (7/12) && age < 1)
+    {
+      return weight * .04;
+    }
+    else 
+    {
+      console.log("Your dog is younger than 2 months!");
+    }
+  }
+}
+
+console.log(dogFeeder(15, 1));
 
 
 
@@ -60,29 +114,56 @@
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
-  
+function rpsGame(yourChoice) {
+
+  let yourChoiceValue = 0.5;
+  let computerChoiceValue = Math.round(Math.random());
+
+  if (yourChoice === 'rock' || yourChoice === 'paper' || yourChoice === 'scissors') {
+    if(yourChoiceValue < computerChoiceValue) {
+      return console.log('You lose...T_T')
+    } else {
+      return console.log('You WIN! ^^')
+    }
+  }
+}
+
+rpsGame('rock');
+rpsGame('paper');
+rpsGame('scissors');
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
-
-
+function convKmToMiles (kilo) {
+  return console.log(kilo * 0.621371 + ' miles');
+}
+convKmToMiles(2.3);
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
   
-
-
+function convFtToCm (feet) {
+  return console.log(feet * 30.48 + ' cm');
+}
+convFtToCm(2.3);
 
 
 /************************************************************** Task 6 **************************************************************/
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
+function annoyingSong (numBottles) {
+  console.log(`${numBottles} bottles on the wall`)
 
+  for(let i = 1; i <= numBottles; i++) {
+    let numBottlesLeft = numBottles - i;
+    console.log(`${i} bottles passed around and ${numBottlesLeft} bottles of soda on the wall left.`);
+
+  }
+}
+annoyingSong(10);
 
 
 
@@ -95,7 +176,23 @@
 //60s should be D 
 //and anything below 60 should be F
   
+function yourGrade (score) {
+  if(score >= 90) {
+    console.log('Your grade: A');
+  } else if (score < 90 && score >= 80) {
+    console.log('Your grade: B');
+  } else if (score < 80 && score >= 70) {
+    console.log('Your grade: C');
+  } else if (score < 70 && score >= 60) {
+    console.log('Your grade: D');
+  } else if (score < 60) {
+    console.log('Your grade: F')
+  } else {
+    console.log('Please type your score between 0 to 100');
+  }
+}
 
+yourGrade(100);
   
   
 
@@ -103,7 +200,21 @@
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
 // Hint - you may need to study tomorrow's traning kit on arrays 
 // try looking up the .includes() method
+const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 
+function countVowels(sentence) {
+  let counts = 0;
+  for(let i = 0; i < vowels.length; i++) {
+    if(vowels.includes(sentence[i])) {
+      counts++;
+    }
+  }
+  return console.log(counts);
+}
+
+countVowels('Hello World');
+countVowels('AaEeIiOoUu');
+countVowels('aaaaa');
 
 
 
@@ -112,6 +223,21 @@
 //Take Rock, Paper, Sissors further
 //update your rock papers sissors code below to take a prompt from a user using the window object
 
+function rpsGame(yourChoice) {
+
+  let yourChoiceValue = 0.5;
+  let computerChoiceValue = Math.round(Math.random());
+
+  if (yourChoice === 'rock' || yourChoice === 'paper' || yourChoice === 'scissors') {
+    if(yourChoiceValue < computerChoiceValue) {
+      return console.log('You lose...T_T')
+    } else {
+      return console.log('You WIN! ^^')
+    }
+  }
+}
+
+rpsGame(prompt('Choose from rock, paper, scissors'))
 
 
 
